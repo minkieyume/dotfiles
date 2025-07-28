771c9da57b499c0d259a5c2d1a865d016d2a9fea
MinkieYume <sign@yumieko.com>
2025-07-28 17:46:07 +0800

    channels

--- channels.scm
+++ channels.scm
@@ -25,4 +25,8 @@
 	      "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
 	      (openpgp-fingerprint
 	        "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+  (channel
+    (name 'glicid)
+    (url "https://gitlab.univ-nantes.fr/glicid-public/guix-glicid.git")
+    (branch "main"))
   %default-channels)
