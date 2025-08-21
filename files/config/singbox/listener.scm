0d13cf415bd89043ac2ceb145abec49cc08943e3
MinkieYume <sign@yumieko.com>
2025-08-21 19:22:44 +0800

    wwww

--- files/config/singbox/listener.scm
+++ files/config/singbox/listener.scm
@@ -30,7 +30,7 @@
          ("servers"
            . #((("type" . "https")
                  ("tag" . "cloudflare")
-                 ("server" . "https://1.1.1.1")
+                 ("server" . "1.1.1.1")
 		 ("detour" . "vless_out"))
 	       (("type" . "udp")
 		("tag" . "tailscale_dns")
