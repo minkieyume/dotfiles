0df48858a62af3cae95388336a71c4f4bda0c4ba
MinkieYume <sign@yumieko.com>
2025-08-30 17:06:35 +0800

    emacs_配置

--- files/config/emacs/init.el
+++ files/config/emacs/init.el
@@ -261,6 +261,29 @@
 	 alltodo ""
          ((org-agenda-files '("~/LocalWork/程序开发/LiquidNeko/features.org")))
 	 (org-agenda-use-tag-inheritance t))))
+(use-package cmake-mode)
+
+(use-package cmake-ide
+  :straight t
+  :bind (:map c-mode-map
+	      ("C-c C-r" . cmake-ide-compile))
+  :config
+  (cmake-ide-setup))
+;; 基本语法高亮 & 缩进支持
+(add-hook 'c++-mode-hook #'electric-pair-mode)
+(add-hook 'c++-mode-hook #'show-paren-mode)
+(add-hook 'c++-mode-hook #'display-line-numbers-mode)
+(add-hook 'c++-mode-hook #'indent-tabs-mode)
+
+;; 设置基本缩进宽度
+(setq-default c-basic-offset 4)
+
+;; 启动 GDB 时开启多窗口布局
+(setq gdb-many-windows t
+      gdb-show-main t)
+
+(add-hook 'c-mode-hook #'eglot-ensure)
+(add-hook 'c++-mode-hook #'eglot-ensure)
 (use-package gdscript-mode
   :hook (gdscript-mode . eglot-ensure)
   :init
