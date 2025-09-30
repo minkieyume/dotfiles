599f2fb8dfe1e188d8246bcfb014773007ef7a46
MinkieYume <sign@yumieko.com>
2025-09-30 21:02:10 +0800

    修复回环问题

--- channels.scm
+++ channels.scm
@@ -18,6 +18,10 @@
 	   (openpgp-fingerprint
 	    "13E7 6CD6 E649 C28C 3385  4DF5 5E5A A665 6149 17F7"))))
 	(channel
+         (name 'ollama-guix)
+         (url "https://codeberg.org/tusharhero/ollama-guix"))
+	
+	(channel
 	 (name 'nonguix)
 	 (url "https://github.com/nonguix/nonguix.git")
 	 ;; Enable signature verification:
