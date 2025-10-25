09ed7a30dd77b4f80cc651b7693a22a72dddc664
MinkieYume <sign@yumieko.com>
2025-10-25 16:21:03 +0800

    feat(channels): add channels-251024.scm with multiple channel definitions
    feat(system): add cron job to backup /var/lib/synapse via rsync
    
    ---
    
    feat(channels): 新增 channels-251024.scm，包含多个 channel 定义
    
    feat(system): 新增定时任务，使用 rsync 备份 /var/lib/synapse 目录

---
+++ channels/channels-251024.scm
@@ -0,0 +1,86 @@
+(list (channel
+        (name 'chikochannel)
+        (url "https://github.com/minkieyume/chiko-guix-channel.git")
+        (branch "main")
+        (introduction
+          (make-channel-introduction
+            "32fb2e5201db0995261f0329081901ac048dcbd0"
+            (openpgp-fingerprint
+              "1D49 47B7 EC8F 8C3F 4B19  7077 9E91 3592 5221 70AD"))))
+      (channel
+        (name 'rosenthal)
+        (url "https://codeberg.org/hako/rosenthal.git")
+        (branch "trunk")
+        (commit
+          "1c3aed0cdb212a47a2d7619ac3aec103344f4f13")
+        (introduction
+          (make-channel-introduction
+            "7677db76330121a901604dfbad19077893865f35"
+            (openpgp-fingerprint
+              "13E7 6CD6 E649 C28C 3385  4DF5 5E5A A665 6149 17F7"))))
+      (channel
+        (name 'nonguix)
+        (url "https://github.com/nonguix/nonguix.git")
+        (branch "master")
+        (commit
+          "a345ef84fbdf3b2491acb2c2b6665a4eb97bd4aa")
+        (introduction
+          (make-channel-introduction
+            "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
+            (openpgp-fingerprint
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+      (channel
+        (name 'guix-science-nonfree)
+        (url "https://codeberg.org/guix-science/guix-science-nonfree.git")
+        (branch "master")
+        (commit
+          "baee36c468c8b23cff98ae90d6598291547b869f")
+        (introduction
+          (make-channel-introduction
+            "58661b110325fd5d9b40e6f0177cc486a615817e"
+            (openpgp-fingerprint
+              "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
+      (channel
+        (name 'guixcn)
+        (url "https://codeberg.org/guixcn/guix-channel.git")
+        (branch "master")
+        (commit
+          "b29cd6188fd63aafb06693902cff5ff089bad392")
+        (introduction
+          (make-channel-introduction
+            "993d200265630e9c408028a022f32f34acacdf29"
+            (openpgp-fingerprint
+              "7EBE A494 60CE 5E2C 0875  7FDB 3B5A A993 E1A2 DFF0"))))
+      (channel
+        (name 'guix)
+        (url "https://git.guix.gnu.org/guix.git")
+        (branch "master")
+        (commit
+          "f3247d4ce2436e155aad9522f032d0962f0d4eab")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
+      (channel
+        (name 'guix-science)
+        (url "https://codeberg.org/guix-science/guix-science.git")
+        (branch "master")
+        (commit
+          "cad6e8c33f08efd1aab96cc76bd1a26550851d2f")
+        (introduction
+          (make-channel-introduction
+            "b1fe5aaff3ab48e798a4cce02f0212bc91f423dc"
+            (openpgp-fingerprint
+              "CA4F 8CF4 37D7 478F DA05  5FD4 4213 7701 1A37 8446"))))
+      (channel
+        (name 'guix-past)
+        (url "https://codeberg.org/guix-science/guix-past.git")
+        (branch "master")
+        (commit
+          "c3bc94ee752ec545e39c1b8a29f739405767b51c")
+        (introduction
+          (make-channel-introduction
+            "0c119db2ea86a389769f4d2b9c6f5c41c027e336"
+            (openpgp-fingerprint
+              "3CE4 6455 8A84 FDC6 9DB4  0CFB 090B 1199 3D9A EBB5")))))
