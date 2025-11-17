f35283441a39d0e3448d1dc17f2aa5ce78db30cd
MinkieYume <sign@yumieko.com>
2025-11-18 01:58:05 +0800

    feat(desktop): add desktop package and set modules
    新增 desktop 相关的 package 和 set 模块，包含 chiko-modules/packages/desktop.scm 和 chiko-modules/sets/desktop.scm 文件，为桌面环境配置提供基础。
    
    ---
    
    feat(desktop): add desktop package and set modules
    
    Added chiko-modules/packages/desktop.scm and chiko-modules/sets/desktop.scm to provide basic modules for desktop environment configuration.

---
+++ build/build-chikoyumemi.scm
@@ -0,0 +1,4 @@
+(add-to-load-path (string-append (getcwd)))
+(use-modules (chiko-modules machine chikoyumemi))
+
+(make-chikoyumemi-os)
---
+++ chiko-modules/packages/desktop.scm
@@ -0,0 +1,5 @@
+(define-module (chiko-modules packages desktop)
+  #:use-module (rosenthal)
+  #:use-module (guix profiles)
+  #:export (%emacs-packages))
+
