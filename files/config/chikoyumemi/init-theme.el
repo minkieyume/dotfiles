f63dd1826af3c7ebc18c540904ea42412a418b4a
MinkieYume <sign@yumieko.com>
2025-12-03 20:06:31 +0800

    www

--- files/config/chikoyumemi/init-theme.el
+++ files/config/chikoyumemi/init-theme.el
@@ -6,4 +6,14 @@
 		    :family "Sarasa Term SC"
 		    :height 120)
 
+;; 向量数据库：chikoyumemi 限定
+(use-package vecdb
+  :config
+  (require 'vecdb-qdrant)
+  (defvar chiko-vecdb-provider (make-vecdb-qdrant-provider :api-key (string-trim (with-temp-buffer
+										   (insert-file-contents "$$qdrant-api-key.txt$$")
+										   (buffer-string)))
+  							 :url "http://chikoyumemi:6333"))
+  (setq ekg-vecdb-provider (cons chiko-vecdb-provider (make-vecdb-collection :name "ekg" :vector-size 1024))))
+
 (provide 'init-theme)
