11b5d437ffa1f6c2cae83bee3d0e64d27a0269b2
MinkieYume <sign@yumieko.com>
2025-12-30 21:54:29 +0800

    misskey updated

--- files/config/singbox/listener.scm
+++ files/config/singbox/listener.scm
@@ -92,7 +92,7 @@
       (("ip_cidr" . #("140.82.112.0/20" "140.82.121.0/20" "192.30.252.0/22" "185.199.108.0/22" "143.55.64.0/20")))
       (("ip_cidr" . #("2600:1f18::/32")))
       (("ip_cidr" . #("2a01:4f9::/32")))
-      (("domain_suffix" . "boiledscript.com"))
+      (("domain_suffix" . #("boiledscript.com" "chn.moe" "korange.work")))
       (("domain_suffix" . "freedesktop.org"))
       (("domain_suffix" . "allthemusic.info"))
       (("domain_suffix" . #("guix.moe")))
